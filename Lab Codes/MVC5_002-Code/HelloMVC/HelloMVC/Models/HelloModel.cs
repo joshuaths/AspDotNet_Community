1e3c2728de3f0a3eb5dbe2a18fe5e5a6f7408f22
Chester Chin <chinschian@hotmail.com>
2018-01-30 10:34:47 +0800

    Added challenge codes and solutions

--- Lab Codes/MVC5_002-Code/HelloMVC/HelloMVC/Models/HelloModel.cs
+++ Lab Codes/MVC5_002-Code/HelloMVC/HelloMVC/Models/HelloModel.cs
@@ -24,6 +24,7 @@ namespace HelloMVC.Models
         public int Id { get; set; }
         public string Make { get; set; }
         public string Model { get; set; }
+
     }
 
 }
---
+++ MVC-Challenge-Code/FirstChallenge/FirstChallenge/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+﻿using FirstChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstChallenge.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult Index()
+        {
+
+            var alotofBooks = ComicBookManager.GetComicBooks();
+            return View(alotofBooks);
+
+
+        }//End of Index
+
+
+        public ActionResult Details(int id)
+        {
+
+            var alotOfComic = ComicBookManager.GetComicBooks();
+
+            //We actually see this before. FirstOrdefault, later we will revisit Entity Framework to find it. 
+            
+            //Find the first book based on id given
+            var onlyOneComic = alotOfComic.FirstOrDefault(p => p.ComicBookId == id);
+
+
+            return View(onlyOneComic);
+        }
+    }
+}
